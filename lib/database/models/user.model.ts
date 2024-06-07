0b39d8b5a47c763ac744905b68e989984ba6e4d8
sandeeps79 <sandeep.sanwale@opensenselabs.com>
2024-06-07 11:29:09 +0530

    initial commit

---
+++ lib/database/models/user.model.ts
@@ -0,0 +1,27 @@
+import { Document, Schema, model, models } from "mongoose";
+
+export interface IUser extends Document {
+  clearId: string;
+  email: string;
+  username: string;
+  photo: string;
+  firstName?: string;
+  lastName?: string;
+  planId: number;
+  creditBalance: number;
+}
+
+const UserSchema = new Schema({
+  clearId: { type: String, required: true, unique: true },
+  email: { type: String, required: true, unique: true },
+  username: { type: String, required: true, unique: true },
+  photo: { type: String, required: true },
+  firstName: { type: String },
+  lastName: { type: String },
+  planId: { type: Number, default: 1 },
+  creditBalance: { type: Number, default: 10 },
+});
+
+const User = models?.User || model("User", UserSchema);
+
+export default User;
